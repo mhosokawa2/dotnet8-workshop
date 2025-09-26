c0b2a645227ca2d8e517d0cbfd26f4103e5be845
JBS-Mamoru, Hosokawa <mamoru.hosokawa@jbs.com>
2025-09-26 15:54:08 +0900

    :sparkles: Add CodeReview Samples

---
+++ samples/code-review-demo/code-review-demo/CodeReviewBefore.cs
@@ -0,0 +1,27 @@
+﻿namespace code_review_demo
+{
+    /// <summary>
+    /// コードレビュー前のサンプルクラス
+    /// </summary>
+    public class CodeReviewBefore
+    {
+        // データを処理するメソッド
+        public void process(List<string?> data)
+        {
+            if (data != null)
+            {
+                for (int i = 0; i < data.Count; i++)
+                {
+                    var s = data[i];
+                    if (s.Length > 0)
+                    {
+                        s = s.Trim();
+                        var processed = s.ToLower() + DateTime.Now;
+                        Console.WriteLine("処理結果: " + processed);
+                        if (processed.Contains("error")) return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ samples/code-review-demo/code-review-demo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using code_review_demo;
+
+new CodeReviewBefore().process(["hoge", "fuga", "hogefuga"]);
+CodeReviewAfter.ProcessData(["hoge", "fuga", "hogefuga"]);
